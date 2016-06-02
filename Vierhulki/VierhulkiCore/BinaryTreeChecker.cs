e716d0415ea04987ecdb3d258cf6cecfa984f4a2
DamianRutkiewicz <damian.rutkiewicz@interia.pl>
2016-06-02 16:22:51 +0200

    wstepnie napisany algorytm sprawdzania

--- Vierhulki/VierhulkiCore/BinaryTreeChecker.cs
+++ Vierhulki/VierhulkiCore/BinaryTreeChecker.cs
@@ -8,6 +8,25 @@ namespace VierhulkiCore
 {
     public class BinaryTreeChecker
     {
+        List<Vertex> visited = new List<Vertex>();
 
+        public bool Check(Vertex w)
+        {
+            Boolean isbinary = true;
+            visited.Add(w);
+            foreach (var item in w.NextVertexes)
+            {
+                if (w.NextVertexes.Count > 3)
+                {
+                    isbinary = false;
+                }
+                if (!w.NextVertexes.Contains(item))
+                {
+                    Check(item);
+                }
+            }            
+          return isbinary;
+            Console.WriteLine(isbinary);
+        }
     }
 }
