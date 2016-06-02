cc26d2023b7338e3e3131e4b16675d4d57e3e929
Niviti <rafak@o2.pl>
2016-06-02 16:49:16 +0200

    metoda tworząca graf z macierzy sąsiedztwa

---
+++ Vierhulki/VierhulkiCore/GraphCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VierhulkiCore
+{
+    class GraphCreator
+    {
+
+        public static void CreateFromMatrix(Vertex[] vertexes, int[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException("Macierz musi być kwadratowa!");
+            }
+
+            int length = matrix.GetLength(0);
+            
+            for (int i = 0; i < length; i++)
+            {
+                vertexes[i].NextVertexes = new List<Vertex>();
+                for (int j = 0; j < length; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        vertexes[i].NextVertexes.Add(vertexes[j]);
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Vierhulki/VierhulkiCore/Vertex.cs
+++ Vierhulki/VierhulkiCore/Vertex.cs
@@ -6,8 +6,47 @@ using System.Threading.Tasks;
 
 namespace VierhulkiCore
 {
+
+
+
+    class Węzeł
+    {
+        public String wartość;
+        public Węzeł lewy;
+        public Węzeł prawy;
+    }
+    class Drzewo
+    {
+        public Węzeł korzeń;
+
+
+        static Węzeł UtwórzWęzeł(String wartość)
+        {
+            Węzeł węzeł = new Węzeł();
+            węzeł.wartość = wartość;
+            return węzeł;
+        }
+        static void DodajLewy(Węzeł węzeł, Węzeł dziecko)
+        {
+            węzeł.lewy = dziecko;
+        }
+        static void DodajPrawy(Węzeł węzeł, Węzeł dziecko)
+        {
+            węzeł.prawy = dziecko;
+        }
+
+
+    }
+
+
+
     public class Vertex
     {
         public List<Vertex> NextVertexes { get; set; }
+
+
+
     }
+
+
 }
